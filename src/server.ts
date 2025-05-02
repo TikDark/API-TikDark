2cd1f9d53a41faefcd2bc52112210e41d1a4bd47
Gustavo Pereira <gustavo.cabral.campos1@gmail.com>
2025-05-02 18:40:29 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,30 @@
 import dotenv from 'dotenv';
-dotenv.config(); // Carrega as variáveis do arquivo .env
+dotenv.config(); 
 
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { stripeRouter } from './routes/stripeRoutes';
-import { connectDB } from './config/db'; // Importa a função de conexão
+import { connectDB } from './config/db'; 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Conecta ao MongoDB
 connectDB();
 
-// Middleware para ler o corpo das requisições como JSON
+
 app.use(cors());
 app.use(bodyParser.json());
 
-// Rotas
+
 app.use('/api', stripeRouter);
 
-// Rota de teste
+
 app.get('/', (req, res) => {
   res.send('Servidor rodando');
 });
 
-// Inicia o servidor
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
