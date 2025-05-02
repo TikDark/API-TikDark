3c5bf3c46c57c70265ac06f08bcd30b60e6a94a9
Gustavo Pereira <gustavo.cabral.campos1@gmail.com>
2025-05-02 18:39:45 -0300

    Update stripe.ts

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -1,14 +1,13 @@
-// src/utils/stripe.ts
+
 import Stripe from 'stripe';
 
-// Verifica se a chave da API está definida no arquivo .env
+
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 
 if (!stripeSecretKey) {
   throw new Error("STRIPE_SECRET_KEY is missing");
 }
 
-// Inicializa o Stripe com a chave da API
 const stripe = new Stripe(stripeSecretKey, { apiVersion: '2024-12-18.acacia' });
 
 export { stripe };
